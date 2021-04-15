db1e7cd9c18c1ac315788f37cba52bae068d683a
Jarataca <64710811+Jarataca@users.noreply.github.com>
2021-04-15 01:22:12 -0600

    Intefaz Menu principal

---
+++ SC-MMascotass/MenuPrincipal.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SC_MMascotass
+{
+    /// <summary>
+    /// Interaction logic for MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicio_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AgregarForm(UserControl control) 
+        {
+            this.spPrincipal.Children.Clear();
+            this.spPrincipal.Children.Add(control);
+        }
+    }
+}
