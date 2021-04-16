b0187a750310242a689c1ccd7635498ea89f61fc
Elvis-R-Alfaro <82235597+Elvis-R-Alfaro@users.noreply.github.com>
2021-04-16 14:39:15 -0600

    Creacion de los funciones y constructores del formulario mascotas

--- SC-MMascotass/Pages/FormMascotas.xaml.cs
+++ SC-MMascotass/Pages/FormMascotas.xaml.cs
@@ -19,20 +19,30 @@ namespace SC_MMascotass.Pages
     /// </summary>
     public partial class FormMascotas : Window
     {
-        public FormMascotas()
+        private Mascota mascota = new Mascota();
+        public static int ides;
+        public FormMascotas(bool visible)
         {
             InitializeComponent();
 
             var border = (resultStack.Parent as ScrollViewer).Parent as Border;
             border.Visibility = System.Windows.Visibility.Collapsed;
+
+            if (ides != 0)
+            {
+                //mascota = mascota.BuscarMascota(ides);
+                //txtAuCliente.Text = mascota.IdCliente;
+            }
         }
+
 
         //Autocompletar TextBox
         private void txtAuCliente_KeyUp(object sender, KeyEventArgs e)
         {
             bool found = false;
             var border = (resultStack.Parent as ScrollViewer).Parent as Border;
-            var data = ClientesCS.GetData();
+            var data = Mascota.MonstrarMascotas22();
+          
 
             string query = (sender as TextBox).Text;
 
@@ -81,7 +91,10 @@ namespace SC_MMascotass.Pages
             // Mouse events   
             block.MouseLeftButtonUp += (sender, e) =>
             {
+                var border = (resultStack.Parent as ScrollViewer).Parent as Border;
                 txtAuCliente.Text = (sender as TextBlock).Text;
+                border.Visibility = System.Windows.Visibility.Collapsed;
+                ObtenerValoresFormulario();
             };
 
             block.MouseEnter += (sender, e) =>
@@ -105,6 +118,70 @@ namespace SC_MMascotass.Pages
             // Mostrar el formulario de menú principal
             FormCliente cliente = new FormCliente();
             cliente.Show();
+        }
+
+        private bool VerificarValores()
+        {
+            if (txtAuCliente.Text == string.Empty)
+            {
+                MessageBox.Show("!Ingrese el Nombre del Cliente¡");
+                return false;
+            }
+            else if (txtAliasMascota.Text == string.Empty)
+            {
+                MessageBox.Show("!Ingrese el Nombre de la Mascota¡");
+                return false;
+            }
+            else if (txtColorPelo.Text == string.Empty)
+            {
+                MessageBox.Show("!Ingrese el Color de Pelo de la mascota¡");
+                return false;
+            }
+            else if (txtEspecie.Text == string.Empty)
+            {
+                MessageBox.Show("!Ingrese la especie de la Mascota¡");
+                return false;
+            }
+            else if (txtRaza.Text == string.Empty)
+            {
+                MessageBox.Show("!Ingrese la raza de la Mascota¡");
+                return false;
+            }
+            return true;
+        }
+
+        private void ObtenerValoresFormulario()
+        {
+            //categoria.NombreCategoria = txtCategoria.Text;
+            mascota = mascota.ObtenerID(txtAuCliente.Text);
+            MessageBox.Show(mascota);
+        }
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (VerificarValores())
+            {
+                try
+                {
+                    //Obtener los valores para la habitacion
+                    ObtenerValoresFormulario();
+                    
+
+                    //Insertar los datos de la habitacion
+                    //categoria.CrearCategoria(categoria);
+
+                    //Mensaje de inserccion exito
+                    MessageBox.Show("Datos insertados correctamente");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ha ocurrido un error al momento de insertar la habitacion....");
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    //txtCategoria.Text = string.Empty;
+                }
+            }
         }
     }
 }
--- SC-MMascotass/Pages/Mascotas.xaml.cs
+++ SC-MMascotass/Pages/Mascotas.xaml.cs
@@ -20,16 +20,75 @@ namespace SC_MMascotass.Pages
     /// </summary>
     public partial class Mascotas : UserControl
     {
+        private Mascota mascota = new Mascota();
+        private List<Mascota> mascotas;
         public Mascotas()
         {
             InitializeComponent();
+
+            ObtenerMascotas();
         }
 
         private void btnNuevo_Click(object sender, RoutedEventArgs e)
         {
             // Mostrar el formulario de menú principal
-            FormMascotas mascota = new FormMascotas();
+            FormMascotas mascota = new FormMascotas(true);
             mascota.Show();
+        }
+
+        private void ObtenerMascotas()
+        {
+            mascotas = mascota.MonstrarMascotas();
+            dgClientes.SelectedValuePath = "IdCliente";
+            dgClientes.ItemsSource = mascotas;
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgClientes.SelectedValue == null)
+                MessageBox.Show("Por favor selecciona una Mascota");
+            else
+            {
+                //FormMascotas.ides = Convert.ToInt32(dgClientes.SelectedValue);
+                FormMascotas mascota = new FormMascotas(false);
+                mascota.Show();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dgClientes.SelectedValue == null)
+                    MessageBox.Show("Por favor selecciona una Mascota desde el listad");
+                else
+                {
+                    //Monstrar mensjae de confirmacion
+                    MessageBoxResult result = MessageBox.Show("¿Deseas eliminar la mascota?", "Confirmar", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        //Eliminar la habitacion
+                        mascota.EliminarMascota(Convert.ToInt32(dgClientes.SelectedValue));
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error al eliminar la habitacion...");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Actualizar el listbox de habitaciones
+                ObtenerMascotas();
+            }
+        }
+
+        private void btnRefrescar_Click(object sender, RoutedEventArgs e)
+        {
+            ObtenerMascotas();
         }
     }
 }
