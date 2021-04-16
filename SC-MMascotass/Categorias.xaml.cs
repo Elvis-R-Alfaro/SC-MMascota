498485c9ba1f95d68726a8e2502e7b91c9fa7da0
Jarataca <64710811+Jarataca@users.noreply.github.com>
2021-04-15 18:21:31 -0600

    Formulario Categorias y VIsta del menu principal

---
+++ SC-MMascotass/Categorias.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SC_MMascotass.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SC_MMascotass
+{
+    /// <summary>
+    /// Interaction logic for Categorias.xaml
+    /// </summary>
+    public partial class Categorias : UserControl
+    {
+        public Categorias()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            FormCategorias categoria = new FormCategorias();
+            categoria.Show();
+        }
+    }
+}
